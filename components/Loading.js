0e9fa9f8e073fb0ab1e076dc29a8d4981bcdd45a
yashbhatia9 <mr.yash12345@gmail.com>
2021-05-29 23:43:18 +0530

    loading logo changed

--- components/Loading.js
+++ components/Loading.js
@@ -5,7 +5,7 @@ function Loading() {
         <center style={{ display: "grid", placeItems: "center", height: "100vh"}}>
             <div>
                 <img 
-                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+                src="https://avatars.githubusercontent.com/u/85031310?v=4"
                 alt=""
                 style={{ marginBottom: 10 }}
                 height={200}
